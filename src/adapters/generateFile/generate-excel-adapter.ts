9990515f28a44ae05f497a34a51d9abd3218a5d6
João Pedro <jjoao.monteiro15@gmail.com>
2023-01-21 20:14:33 -0300

    chore: rename file and types generate-excel-adapter

---
+++ src/adapters/generateFile/generate-excel-adapter.ts
@@ -0,0 +1,30 @@
+import { Workbook } from 'exceljs'
+import { GenerateFileProtocol } from '../generate-file-protocol'
+
+export class GenerateExcelAdapter implements GenerateFileProtocol {
+  public readonly workbook = new Workbook()
+  constructor(){}
+  async generate(headers: {}[], data: {}[]) {
+    const sheet = this.workbook.addWorksheet('Primeira Aba')
+    sheet.columns = headers
+
+    this.rows(data, sheet)
+
+    sheet.getRow(1).font = {
+      bold: true,
+      color: { argb: 'FFCCCCCC' }
+    }
+    sheet.getRow(1).fill ={
+      type: 'pattern',
+      pattern: 'solid',
+      bgColor: { argb: 'FF000000' }
+    }  
+    await sheet.workbook.xlsx.writeFile('test.xlsx')
+  }
+
+  private rows(data:{}[], sheet:any){
+    for(let item of data) {
+      sheet.addRow(data)
+    }
+  }
+}
