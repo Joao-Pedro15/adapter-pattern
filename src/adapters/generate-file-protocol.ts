6edefe6df145381d082ad7e1dfb4eb2da5305148
João Pedro <jjoao.monteiro15@gmail.com>
2023-01-30 20:56:41 -0300

    feat: create class adapter from generate files

--- src/adapters/generate-file-protocol.ts
+++ src/adapters/generate-file-protocol.ts
@@ -8,3 +8,20 @@ export interface GenerateFileProtocol {
 export interface GenerateFileFn {
   (headers: string[] | {}[], data: {}[]): Promise<any>
 }
+
+export interface GenerateFileTarget {
+  csvFile: GenerateFileFn
+  excelFile: GenerateFileFn
+}
+
+export class GenerateFileAdapter implements GenerateFileTarget {
+  async csvFile(headers: string[] | {}[], data: {}[]) {
+    return await new GenerateCsvAdapter().generate(headers, data)
+  }
+
+  async excelFile(headers: string[] | {}[], data: {}[]) {
+    return await new GenerateExcelAdapter().generate(headers, data)
+  }
+
+  
+}
